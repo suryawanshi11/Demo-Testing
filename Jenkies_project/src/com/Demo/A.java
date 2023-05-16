c4417a73963de245c102af17283d54d0cf90c4e3
diwyaratna <diwyaratna@LAPTOP-KH44GH5A>
2023-05-16 11:31:50 +0530

    By AmitSir

---
+++ Jenkies_project/src/com/Demo/A.java
@@ -0,0 +1,21 @@
+package com.Demo;
+
+public class A
+{
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+
+		
+		System.out.println("done");
+		
+		System.out.println("=========");
+		
+		System.out.println("====**********=====");
+		
+	}
+	public void m1()
+	{
+		System.out.println("dhsdvs");
+	}
+
+}
